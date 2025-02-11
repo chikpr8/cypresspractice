88c1cd4961184296e72f99420e8ae36191eae982
chikpr8 <chikkala66@gmail.com>
2025-02-11 05:46:35 +0530

    cypress

---
+++ cypress/e2e/Wrape.cy.js
@@ -0,0 +1,9 @@
+describe("Testscenario", () => {
+  before("test", () => {
+    cy.visit("/web/index.php/auth/login");
+    cy.get("[placeholder='Username']").type("Admin");
+    cy.get("[placeholder='Password']").type("admin123");
+    cy.get("[type='submit']").click();
+  });
+  it("logintest", () => {});
+});
