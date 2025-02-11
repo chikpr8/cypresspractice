41cee3e7d6632f298efd23a052b4c4abec9188a8
chikpr8 <chikkala66@gmail.com>
2025-02-11 06:18:00 +0530

    Adding git actions

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,15 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "https://opensource-demo.orangehrmlive.com",
+    screenshotsFolder: "cypress/screenshots", // Screenshots directory
+    videosFolder: "cypress/videos", // Video recordings directory
+    chromeWebSecurity: false, // Disable security checks (useful for cross-origin tests)
+    defaultCommandTimeout: 10000, // Time before a Cypress command fails
+    pageLoadTimeout: 60000, // Maximum time for a page to load
+    viewportWidth: 1280, // Set browser viewport width
+    viewportHeight: 720, // Set browser viewport height
+    retries: 2, // Number of retries on test failure
+    trashAssetsBeforeRuns: true,
   },
+  video: true,
 });
--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -1,9 +1,8 @@
 describe("Testscenario", () => {
-  it("logintest", () => {
+  it.skip("logintest", () => {
     cy.visit("/web/index.php/auth/login");
     cy.get("[placeholder='Username']").type("Admin");
     cy.get("[placeholder='Password']").type("admin123");
     cy.get("[type='submit']").click();
-    
   });
 });
